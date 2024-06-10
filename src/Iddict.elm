80ce9e4927fc8fc6a65b404a215cad993fe3f671
Bram <bram@noordstar.me>
2024-06-10 17:33:41 +0200

    Run elm-format

--- src/Iddict.elm
+++ src/Iddict.elm
@@ -35,9 +35,11 @@ do not need to generate identifiers yourself.
 
 @docs keys, values, toList, fromList
 
+
 ## Transform
 
 @docs map, foldl, foldr, filter, partition
+
 
 ## Combine
 
@@ -82,10 +84,10 @@ decoder coder =
                 }
         )
         (D.field "cursor" D.int)
-        ( D.keyValuePairs coder
+        (D.keyValuePairs coder
             |> D.map
                 (List.filterMap
-                    (\(key, val) ->
+                    (\( key, val ) ->
                         String.toInt key
                             |> Maybe.map (\k -> Tuple.pair k val)
                     )
@@ -94,11 +96,13 @@ decoder coder =
             |> D.field "dict"
         )
 
+
 {-| Keep a key-value pair when its key does not appear in the second id-dict.
 -}
 diff : Iddict a -> Iddict a -> Iddict a
 diff (Iddict old) (Iddict toRemove) =
     Iddict { old | dict = Dict.diff old.dict toRemove.dict }
+
 
 {-| Create an empty id-dict.
 -}
@@ -109,9 +113,10 @@ empty =
         , dict = Dict.empty
         }
 
-{-| Convert an association list into an id-dict. 
+
+{-| Convert an association list into an id-dict.
 -}
-fromList : List (Int, a) -> Iddict a
+fromList : List ( Int, a ) -> Iddict a
 fromList items =
     Iddict
         { cursor =
@@ -121,6 +126,7 @@ fromList items =
                 |> Maybe.withDefault 0
         , dict = Dict.fromList items
         }
+
 
 {-| Encode an id-dict to a JSON value.
 -}
@@ -135,11 +141,13 @@ encode coder (Iddict d) =
           )
         ]
 
+
 {-| Keep only the key-value pairs that pass the given test.
 -}
 filter : (Int -> a -> Bool) -> Iddict a -> Iddict a
 filter f (Iddict d) =
     Iddict { d | dict = Dict.filter f d.dict }
+
 
 {-| Fold over the key-value pairs in an id-dict from lowest key to highest key.
 -}
@@ -147,11 +155,13 @@ foldl : (Int -> a -> b -> b) -> b -> Iddict a -> b
 foldl f start (Iddict d) =
     Dict.foldl f start d.dict
 
+
 {-| Fold over the key-value pairs in an id-dict from highest key to lowest key.
 -}
 foldr : (Int -> a -> b -> b) -> b -> Iddict a -> b
 foldr f start (Iddict d) =
     Dict.foldr f start d.dict
+
 
 {-| Get a value from the id-dict using its key.
 -}
@@ -176,6 +186,7 @@ insert v (Iddict d) =
     , Iddict { cursor = d.cursor + 1, dict = Dict.insert d.cursor v d.dict }
     )
 
+
 {-| Keep a key-value pair when its key appears in the second id-dict.
 Preference is given to values in the first id-dict.
 -}
@@ -185,6 +196,7 @@ intersect (Iddict d1) (Iddict d2) =
         { cursor = Basics.max d1.cursor d2.cursor
         , dict = Dict.intersect d1.dict d2.dict
         }
+
 
 {-| Determine if an id-dict is empty.
 -}
@@ -199,11 +211,13 @@ keys : Iddict a -> List Int
 keys (Iddict { dict }) =
     Dict.keys dict
 
+
 {-| Apply a function to all values in an id-dict.
 -}
 map : (Int -> a -> b) -> Iddict a -> Iddict b
 map f (Iddict d) =
     Iddict { cursor = d.cursor, dict = Dict.map f d.dict }
+
 
 {-| Determine if a key is in an id-dict.
 -}
@@ -211,9 +225,9 @@ member : Int -> Iddict a -> Bool
 member k (Iddict d) =
     k < d.cursor && Dict.member k d.dict
 
+
 {-| The most general way of combining two id-dicts. You provide three
 accumulators for when a given key appears:
-
 
     1. Only in the left id-dict.
     2. In both id-dicts.
@@ -221,13 +235,19 @@ accumulators for when a given key appears:
 
 You then traverse all the keys from lowest to highest, building up whatever you
 want.
+
 -}
-merge : (Int -> a -> result -> result)
+merge :
+    (Int -> a -> result -> result)
     -> (Int -> a -> b -> result -> result)
     -> (Int -> b -> result -> result)
-    -> Iddict a -> Iddict b -> result -> result
+    -> Iddict a
+    -> Iddict b
+    -> result
+    -> result
 merge fA fBoth fB (Iddict a) (Iddict b) start =
     Dict.merge fA fBoth fB a.dict b.dict start
+
 
 {-| Partition an id-dict according to some test. The first id-dict contains all
 key-value pairs which passed the test, and the second contains the pairs that
@@ -240,6 +260,7 @@ partition f (Iddict d) =
             ( Iddict { cursor = d.cursor, dict = p1 }
             , Iddict { cursor = d.cursor, dict = p2 }
             )
+
 
 {-| Remove a key-value pair from the id-dict. If the key is not found, no
 changes are made.
@@ -262,11 +283,13 @@ size : Iddict a -> Int
 size (Iddict d) =
     Dict.size d.dict
 
+
 {-| Convert an id-dict into an association list of key-value pairs, sorted by keys.
 -}
-toList : Iddict a -> List (Int, a)
+toList : Iddict a -> List ( Int, a )
 toList (Iddict d) =
     Dict.toList d.dict
+
 
 {-| Combine two id-dicts. If there is a collision, preference is given to the
 first id-dict.
@@ -277,6 +300,7 @@ union (Iddict d1) (Iddict d2) =
         { cursor = Basics.max d1.cursor d2.cursor
         , dict = Dict.union d1.dict d2.dict
         }
+
 
 {-| Update the value of an id-dict for a specific key with a given function.
 This is also the only option that allows you to insert values at a specific key,
